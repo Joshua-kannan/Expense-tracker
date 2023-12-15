9fc3218ff0005b6113376eaeabec54909c626fc3
joshua-kannan <joshuakannanwork@gmail.com>
2023-12-15 18:21:49 +0530

    css edit

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -2,13 +2,13 @@
     <h3>Add new transaction</h3>
     <form id="form" @submit.prevent="onSubmit">
       <div class="form-control">
-        <label for="text">Text</label>
+        <label for="text">Description</label>
         <input type="text" id="text" placeholder="Enter text..." v-model="text" />
       </div>
       <div class="form-control">
         <label for="amount"
           >Amount <br />
-          (negative - expense, positive - income)</label
+          (For negative use - sign before amount)</label
         >
         <input
           type="text"
