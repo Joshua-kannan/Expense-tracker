cf9966fb0ea378039a17760938400f33b30d94f2
joshua-kannan <joshuakannanwork@gmail.com>
2023-12-15 21:32:14 +0530

    margins

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,11 +14,18 @@
       bottom: 0;
       left: 0; /* Set left to 0 to span the entire width */
       width: 100%;
+      margin-top: 5.6rem;
     }
     footer a{
         color: #7762e0;
         text-decoration: none;
     }
-    
+    @media (max-width: 768px) {
+  /* Adjust styles for screens with a width of 768px or less (typical tablets and smaller screens) */
+  footer {
+    position: relative;
+    margin-bottom: 0;
+  }
+}
   </style>
   
